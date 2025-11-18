38c7336ae13f9b1f8cc3dc4baf721d0817e705b3
Ben Hetland <bhetland4@gmail.com>
2025-11-18 01:57:23 +0100

    Added cpplocal.cpp
    Show how to use a Locale in C++

---
+++ cpplocale.cpp
@@ -0,0 +1,27 @@
+// locale::global example
+#include <iostream>       // std::cout
+#include <locale>         // std::locale
+
+int main (void) {
+  std::locale foo;     // the "C" locale (a copy of the current global)
+  foo.global(std::locale("")); // the global changes (from environment)
+  std::locale bar;     // the "" locale (i.e. from global from env)
+
+  std::cout << "foo and bar are ";
+  std::cout << (foo==bar?"the same":"different");
+  std::cout << ".\n";
+
+  std::locale cla(std::locale::classic());
+  std::locale nn("nn_NO.utf8");
+  std::locale loc("");
+  std::cout << "foo.name: " << foo.name() <<
+             "\nbar.name: " << bar.name() <<
+             "\nnn.name:  " << nn.name() <<
+             "\nloc.name: " << loc.name() <<
+             "\ncla.name: " << cla.name() <<
+             "\nclassic:  " << std::locale::classic().name() <<
+             "\n";
+
+  return 0;
+}
+
